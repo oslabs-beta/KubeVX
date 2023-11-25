6489428c875a3ae90c0b848e25e4e6f383736b7b
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-25 14:50:59 -0800

    complete learnk8s

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,6 +7,8 @@ router.get('/metrics', grafanaController.getMetrics);
 
 router.post('/learnk8s', learnK8sController.addComponent);
 
-router.get('/learnk8s', learnK8sController.getComponent);
+router.get('/learnk8s/:name', learnK8sController.getComponent);
+
+router.get('/clusterview')
 
 module.exports = router;
