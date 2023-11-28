6cdd4d7adf86667a97415867d6c777ebc9d676f9
Pegah Chendari <chendaripegah@gmail.com>
2023-11-28 12:21:20 -0800

    updated styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,28 +5,27 @@ import { UserContext } from '../UserContext';
 const Navigation = () => {
   const { user } = useContext(UserContext);
 
-
-  return (
-    <div className="navigation">
-      <h2 className="nav-item">Kube VX</h2>
-      {user && <div className="user-greeting">Hi {user.username}!</div>}
-      <Link to="/" className="nav-item second-color">
-        Main Dashboard
-      </Link>
-      <Link to="/learnk8s" className="nav-item first-color">
-        Learn Kubernetes
-      </Link>
-      <Link to="/clusterview" className="nav-item third-color">
-        Cluster View
-      </Link>
-      <Link to="/login" className="nav-item third-color">
-        Login
-      </Link>
-      <Link to="/logout" className="nav-item third-color">
-        Logout
-      </Link>
-    </div>
-  );
+  
+return (
+  <div className="navigation">
+    <h2 className="nav-item">Kube VX</h2>
+    {user ? (
+      <div className="user-greeting">Hi {user.username}!</div>
+    ) : (
+      <Link to="/login" className="nav-item third-color">Login</Link>
+    )}
+    
+    <Link to="/" className="nav-item second-color">Main Dashboard</Link>
+    <Link to="/learnk8s" className="nav-item first-color">Learn Kubernetes</Link>
+    <Link to="/clusterview" className="nav-item third-color">Cluster View</Link>
+    
+    {user && (
+      <div className="nav-bottom">
+        <Link to="/logout" className="nav-item third-color">Logout</Link>
+      </div>
+    )}
+  </div>
+);
 };
 
 export default Navigation;
