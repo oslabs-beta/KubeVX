5145552e88560499247d05fbaf181a2cf53545ae
Pegah Chendari <chendaripegah@gmail.com>
2023-11-28 14:39:32 -0800

    last changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,21 +9,17 @@ const Navigation = () => {
 return (
   <div className="navigation">
     <h2 className="nav-item">Kube VX</h2>
-    {user ? (
-      <div className="user-greeting">Hi {user.username}!</div>
-    ) : (
-      <Link to="/login" className="nav-item third-color">Login</Link>
-    )}
-    
+    {user && <div className="nav-item third-color">Hi {user.username}!</div>}
     <Link to="/" className="nav-item second-color">Main Dashboard</Link>
     <Link to="/learnk8s" className="nav-item first-color">Learn Kubernetes</Link>
     <Link to="/clusterview" className="nav-item third-color">Cluster View</Link>
-    
-    {user && (
-      <div className="nav-bottom">
+    <div className="nav-bottom">
+      {user ? (
         <Link to="/logout" className="nav-item third-color">Logout</Link>
-      </div>
-    )}
+      ) : (
+        <Link to="/login" className="nav-item third-color">Login</Link>
+      )}
+    </div>
   </div>
 );
 };
