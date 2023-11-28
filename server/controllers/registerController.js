d75fd9f096109b657b58e90f83c1cf261bbe71f8
Patrick <lepcwdetw@gmail.com>
2023-11-28 15:14:40 -0800

    pwang040: e03su3su;6

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,25 +1,8 @@
 const models = require('../models/componentModel');
 const registerController = {};
 
-registerController.addComponent = async (req, res) => {
-    try {
-      const { name, definition } = req.body;
-      const newComponent = await models.Component.create({ name, definition });
-      res.status(201).json(newComponent);
-    } catch (error) {
-      console.error('Error adding component:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  };
+registerController.register = async (req, res) => {
 
-registerController.getComponent = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const pets = await models.Component.findById(id);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(404).send('Error fetching data');
-  }
-};
+}
 
 module.exports = registerController;
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// define route handler
+// Define route handler
 app.use('/', router);
 
 app.use(express.static(path.join(__dirname, '../src/public')));
