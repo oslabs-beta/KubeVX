2c708c920a1d8b878315cb6b88dc05fa3047bdbe
Patrick <lepcwdetw@gmail.com>
2023-11-28 15:59:21 -0800

    pwang040: removing unnecessary file

--- server/controllers/registerController.js
+++
@@ -1,8 +0,0 @@
-const models = require('../models/componentModel');
-const registerController = {};
-
-registerController.register = async (req, res) => {
-
-}
-
-module.exports = registerController;
