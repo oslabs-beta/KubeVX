8702154e2c3e660eb0060398c8982e8d9ccb2ea0
Pegah Chendari <chendaripegah@gmail.com>
2023-11-28 23:14:33 -0800

    updated some files

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,9 @@
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 
 const Navigation = () => {
   const { user } = useContext(UserContext);
-
   
 return (
   <div className="navigation">
