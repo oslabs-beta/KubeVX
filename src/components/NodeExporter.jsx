5c0c752344b07b5024da9a28ebe00b11dd3d1ce2
Pegah Chendari <chendaripegah@gmail.com>
2023-11-29 20:06:30 -0800

    mainDashboard styling

--- src/components/NodeExporter.jsx
+++ src/components/NodeExporter.jsx
@@ -8,22 +8,22 @@ const NodeExporter = () => {
              <iframe
             title="Grafana Dashboard 5"
             src="http://localhost:3000/d-solo/rYdddlPWk/node-exporter-full?orgId=1&refresh=5s&from=1700612283859&to=now&panelId=77"
-            // width="400"
-            // height="200"
+            width="400"
+            height="200"
           ></iframe>
 
           <iframe
             title="Grafana Dashboard 6"
             src="http://localhost:3000/d-solo/rYdddlPWk/node-exporter-full?orgId=1&refresh=5s&from=1700612230428&to=now&panelId=78"
-            // width="400"
-            // height="200"
+            width="400"
+            height="200"
           ></iframe>
 
           <iframe
             title="Grafana Dashboard 3"
             src="http://localhost:3000/d-solo/rYdddlPWk/node-exporter-full?orgId=1&refresh=5s&from=1700621330570&to=now&panelId=152"
-            // width="400"
-            // height="200"
+            width="400"
+            height="200"
           ></iframe>
 
       </div>
--- src/components/Prometheus.jsx
+++ src/components/Prometheus.jsx
@@ -9,22 +9,22 @@ const Prometheus = () => {
         <iframe
           title="Grafana Dashboard 1"
           src="http://localhost:3000/d-solo/os6Bh8Omk/kubernetes-cluster?orgId=1&from=1701236854560&to=1701237279113&panelId=5"
-          // width="400"
-          // height="200"
+          width="400"
+          height="200"
         ></iframe>
 
         <iframe
           title="Grafana Dashboard 2"
           src="http://localhost:3000/d-solo/os6Bh8Omk/kubernetes-cluster-prometheus?orgId=1&refresh=5s&from=1701215662883&to=now&panelId=14"
-          // width="400"
-          // height="200"
+          width="400"
+          height="200"
         ></iframe>
 
         <iframe
           title="Grafana Dashboard 3"
           src="http://localhost:3000/d-solo/rYdddlPWk/node-exporter-full?orgId=1&from=1701152616604&to=1701239016604&panelId=74"
-          // width="400"
-          // height="200"
+          width="400"
+          height="200"
         ></iframe>
       </div>
     </div>
