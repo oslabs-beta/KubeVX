376ba4f21806da7ab7012e4d9080c1326fd5a964
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-30 19:39:37 -0800

    final mvp >> Co-authored-by: Jerry <jtjerrytrinh@gmail.com> >> Co-authored-by: Pegah <chendaripegah@gmail.com> >> Co-authored-by: Patrick <lepcwdetw@gmail.com> >> Co-authored-by: Jordan <jordansjpalmer@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { UserProvider } from "./UserContext.js";
-import MainDashboard from "./MainDashboard.js";
-import LearnK8s from "./LearnK8s.js";
-import ClusterView from "./ClusterView.js";
-import Login from "./Login.js"; 
-import Register from "./Register.js";
-import Logout from "./Logout.js";
-import AddCluster from "./AddCluster.js"
-import DBoardCreate from "./DBoardCreate.js"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './UserContext.js';
+import MainDashboard from './MainDashboard.js';
+import LearnK8s from './LearnK8s.js';
+import ClusterView from './ClusterView.js';
+import Login from './Login.js';
+import Register from './Register.js';
+import Logout from './Logout.js';
+import AddCluster from './AddCluster.js';
+import DBoardCreate from './DBoardCreate.js';
 
 const App = () => {
   return (
@@ -16,10 +16,10 @@ const App = () => {
       <Router>
         <div>
           <Switch>
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/logout" component={Logout} />
-            <Route exact path="/" component={AddCluster} />
+            <Route exact path="/addcluster" component={AddCluster} />
             <Route exact path="/maindashboard" component={MainDashboard} />
             <Route exact path="/learnk8s" component={LearnK8s} />
             <Route exact path="/clusterview" component={ClusterView} />
--- src/Login.js
+++ src/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
         if (response.status === 200) {
           console.log('Login successful');
           setUser({ username }); // Set user in context
-          history.push('/'); // Redirect to add cluster
+          history.push('/addcluster'); // Redirect to add cluster
         } else {
           const data = await response.json();
           console.error('Login failed:', data.message);
@@ -61,7 +61,7 @@ const Login = () => {
           Create a new account
         </button>
         <div className="back-to-dashboard">
-          <Link to="/">Back to Main Dashboard</Link>
+          <Link to="/addcluster">Back to Main Dashboard</Link>
         </div>
       </form>
     </div>
