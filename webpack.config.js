6acf4680224954f28b288b54325c77b302374b70
Jerry Trinh <jtjerrytrinh@gmail.com>
2023-12-07 18:47:10 -0800

    changed cluster shapes to logos

--- webpack.config.js
+++ webpack.config.js
@@ -81,6 +81,18 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      }
     ],
   },
   plugins: [
